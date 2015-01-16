a08305ebf0b83dfcaa3e74d8e4edbd67c3d2429f
Matthieu Lapeyre <matthieulapeyre@gmail.com>
2015-01-16 15:22:14 +0100

    change default primitive manager filter to sum()

--- software/poppy_humanoid/poppy_humanoid.py
+++ software/poppy_humanoid/poppy_humanoid.py
@@ -1,9 +1,16 @@
+import numpy
+
+from functools import partial
+
 from poppy.creatures import AbstractPoppyCreature
+
 from .primitives.safe import LimitTorque
 
 
 class PoppyHumanoid(AbstractPoppyCreature):
     @classmethod
     def setup(cls, robot):
+        robot._primitive_manager._filter = partial(numpy.sum, axis=0)
+
         robot.attach_primitive(LimitTorque(robot), 'limit_torque')
         robot.limit_torque.start()
