2e47824f45326fd8a48ebb05d424b2933f3ae3e2
Matthieu Lapeyre <matthieulapeyre@gmail.com>
2015-01-16 15:15:17 +0100

    add LimitTorque loop primitive to reduce the torque applied by motors when idle. add this primitive to the setup of PoppyHumanoid robot.

--- software/poppy_humanoid/poppy_humanoid.py
+++ software/poppy_humanoid/poppy_humanoid.py
@@ -1,7 +1,9 @@
 from poppy.creatures import AbstractPoppyCreature
+from .primitives.safe import LimitTorque
 
 
 class PoppyHumanoid(AbstractPoppyCreature):
     @classmethod
     def setup(cls, robot):
-        pass
+        robot.attach_primitive(LimitTorque(robot), 'limit_torque')
+        robot.limit_torque.start()
