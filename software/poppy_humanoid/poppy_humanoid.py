0e173d33cda3db6925bcb37c4ba14fb79029fa43
Matthieu Lapeyre <matthieulapeyre@gmail.com>
2015-01-22 23:35:04 +0100

    attach more primitives during the Poppy Humanoid setup

--- software/poppy_humanoid/poppy_humanoid.py
+++ software/poppy_humanoid/poppy_humanoid.py
@@ -5,6 +5,8 @@ from functools import partial
 from poppy.creatures import AbstractPoppyCreature
 
 from .primitives.safe import LimitTorque
+from .primitives.posture import StandPosition, SitPosition
+from .primitives.dance import SimpleBodyBeatMotion
 
 
 class PoppyHumanoid(AbstractPoppyCreature):
@@ -12,5 +14,13 @@ class PoppyHumanoid(AbstractPoppyCreature):
     def setup(cls, robot):
         robot._primitive_manager._filter = partial(numpy.sum, axis=0)
 
+        for m in robot.motors:
+            m.compliant_behavior = 'safe'
+            m.default_goto_behavior = 'minjerk'
+
+        robot.attach_primitive(StandPosition(robot), 'stand_position')
+        robot.attach_primitive(SitPosition(robot), 'sit_position')
         robot.attach_primitive(LimitTorque(robot), 'limit_torque')
+        robot.attach_primitive(SimpleBodyBeatMotion(robot, 50), 'dance_beat_motion')
+
         robot.limit_torque.start()
