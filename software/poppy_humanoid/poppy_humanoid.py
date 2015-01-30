11c05fbe7c3f2e9f3db6d130ad47f9f50078de3b
Matthieu Lapeyre <matthieulapeyre@gmail.com>
2015-01-30 17:59:09 +0100

    add vrep hack in the PoppyHumanoid setup \o/

--- software/poppy_humanoid/poppy_humanoid.py
+++ software/poppy_humanoid/poppy_humanoid.py
@@ -14,6 +14,8 @@ class PoppyHumanoid(AbstractPoppyCreature):
     def setup(cls, robot):
         robot._primitive_manager._filter = partial(numpy.sum, axis=0)
 
+        if robot.simulated:
+            cls.vrep_hack(robot)
         for m in robot.motors:
             m.compliant_behavior = 'safe'
             m.default_goto_behavior = 'minjerk'
@@ -24,3 +26,11 @@ class PoppyHumanoid(AbstractPoppyCreature):
         robot.attach_primitive(SimpleBodyBeatMotion(robot, 50), 'dance_beat_motion')
 
         # robot.limit_torque.start()
+    @classmethod
+    def vrep_hack(cls, robot):
+        # fix vrep orientation bug
+        wrong_motor = [robot.r_knee_y, robot.abs_x, robot.bust_x]
+
+        for m in wrong_motor:
+            m.direct = not m.direct
+            m.offset = -m.offset
