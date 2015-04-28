6263a2ce9378868dabac56f4e33ccf4586b02982
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-04-28 14:01:24 +0200

    Add the possibility to add forces on any shape of the robot in the vrep simulation.

--- software/poppy_humanoid/poppy_humanoid.py
+++ software/poppy_humanoid/poppy_humanoid.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy
+import ctypes
 
 from functools import partial
 
@@ -20,6 +21,7 @@ class PoppyHumanoid(AbstractPoppyCreature):
 
         if robot.simulated:
             cls.vrep_hack(robot)
+            cls.add_vrep_methods(robot)
 
         for m in robot.motors:
             m.goto_behavior = 'minjerk'
@@ -61,3 +63,24 @@ class PoppyHumanoid(AbstractPoppyCreature):
         for m in wrong_motor:
             m.direct = not m.direct
             m.offset = -m.offset
+
+    @classmethod
+    def add_vrep_methods(cls, robot):
+        from pypot.vrep.controller import VrepController
+        from pypot.vrep.io import remote_api
+
+        def set_vrep_force(robot, vector_force, shape_name):
+            """ Set a force to apply on Poppy Humanoid. """
+            vrep_io = next(c for c in robot._controllers
+                           if isinstance(c, VrepController)).io
+
+            raw_bytes = (ctypes.c_ubyte * len(shape_name)).from_buffer_copy(shape_name)
+            vrep_io.call_remote_api('simxSetStringSignal', 'shape',
+                                    raw_bytes, sending=True)
+
+            packedData = remote_api.simxPackFloats(vector_force)
+            raw_bytes = (ctypes.c_ubyte * len(packedData)).from_buffer_copy(packedData)
+            vrep_io.call_remote_api('simxSetStringSignal', 'force',
+                                    raw_bytes, sending=True)
+
+        robot.set_vrep_force = partial(set_vrep_force, robot)
