d3a8a2c64adbaa96a1a14aadd506b68f38a44db8
Théo Segonds <theo.segonds@inria.fr>
2017-04-10 15:19:05 +0200

    Update setup.py for pypot3

--- software/setup.py
+++ software/setup.py
@@ -10,15 +10,19 @@ def version():
     with open('poppy_humanoid/_version.py') as f:
         return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
 
+
 extra = {}
 if sys.version_info >= (3,):
     extra['use_2to3'] = True
 
+
+# poppy-creature is a placeholder to avoid breaking code examples
+# all its code is now in pypot
 extra_packages = []
 try:
     import poppy.creatures
 
-    extra_packages.append('poppy-creature>=1.9')
+    extra_packages.append('poppy-creature >= 2.0')
 except ImportError:
     pass
 
@@ -26,14 +30,14 @@ setup(name='poppy-humanoid',
       version=version(),
       packages=find_packages(),
 
-      install_requires=['pypot >= 3.0.0a'] + extra_packages,
+      install_requires=['pypot >= 3.0.0'],
 
       include_package_data=True,
       exclude_package_data={'': ['README', '.gitignore']},
 
       zip_safe=False,
 
-      author='Pierre Rouanet, Matthieu Lapeyre',
+      author='See https://github.com/poppy-project/poppy-humanoid/graphs/contributors',
       author_email='pierre.rouanet@gmail.com',
       description='Poppy Humanoid Software Library',
       url='https://github.com/poppy-project/poppy-creature',
