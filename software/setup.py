50d7194c74dfe90d4feea7702a54d9d9157e540b
Yoan Mollard <ymollard@users.noreply.github.com>
2020-06-24 03:22:30 +0200

    Applied definitive 2to3 for python>3.5

--- software/setup.py
+++ software/setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# coding: utf8
 
 import re
 import sys
@@ -12,37 +11,21 @@ def version():
         return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
 
 
-extra = {}
-if sys.version_info >= (3,):
-    extra['use_2to3'] = True
-
-
-# poppy-creature is a placeholder to avoid breaking code examples
-# all its code is now in pypot
-extra_packages = []
-try:
-    import poppy.creatures
-
-    extra_packages.append('poppy-creature >= 2.0')
-except ImportError:
-    pass
-
 setup(name='poppy-humanoid',
       version=version(),
       packages=find_packages(),
 
-      install_requires=['pypot >= 3.0.0'] + extra_packages,
+      install_requires=['pypot >= 4.0.0'],
 
       include_package_data=True,
       exclude_package_data={'': ['README', '.gitignore']},
 
       zip_safe=False,
 
-      author='See https://github.com/poppy-project/poppy-humanoid/graphs/contributors',
-      author_email='pierre.rouanet@gmail.com',
+      author='Poppy Station',
+      author_email='dev@poppy-station.org',
       description='Poppy Humanoid Software Library',
-      url='https://github.com/poppy-project/poppy-creature',
+      url='https://poppy-project.org',
       license='GNU GENERAL PUBLIC LICENSE Version 3',
 
-      **extra
       )
